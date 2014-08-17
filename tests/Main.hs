849188c6faedf61d3928ad5b14d199a2382fad2f
Christopher League <league@contrapunctus.net>
2014-08-17 15:49:49 -0400

    Allow configuration of time zone
    Sometimes we resort to printing the time with the day of week, as in
    "3:50 PM on Sunday". Previously, those times always used UTC. Now you
    can specify a `timeZone` in the `HumanTimeLocale` (default is still
    `utc`).
    
    Includes two new test cases (with two different time zones).

--- tests/Main.hs
+++ tests/Main.hs
@@ -17,6 +17,9 @@ p = fromJust . parseTime defaultTimeLocale "%F %T"
 helper :: String -> String -> String
 helper future past = humanReadableTime' (p future) (p past)
 
+helperI18N :: HumanTimeLocale -> String -> String -> String
+helperI18N ht future past = humanReadableTimeI18N' ht (p future) (p past)
+
 testJustNow = "just now" @=? helper "2013-08-04 22:30:00"
                                     "2013-08-04 22:30:00"
 
@@ -37,6 +40,16 @@ testHoursAgo = "about 4 hours ago" @=? helper "2013-08-05 02:40:00"
 
 testDow = "at 10:30 PM on Sunday" @=? helper "2013-08-08 10:10:10"
                                              "2013-08-04 22:30:00"
+
+testDowEDT = "at 6:30 PM on Sunday" @=?
+             helperI18N defaultHumanTimeLocale{timeZone=read "EDT"}
+             "2013-08-08 10:10:10"
+             "2013-08-04 22:30:00"
+
+testDowIndia = "at 4:00 AM on Monday" @=?
+             helperI18N defaultHumanTimeLocale{timeZone=read "+0530"}
+             "2013-08-08 10:10:10"
+             "2013-08-04 22:30:00"
 
 testDaysAgo = "6 days ago" @=? helper "2013-08-10 22:40:00"
                                       "2013-08-04 22:30:00"
@@ -62,6 +75,8 @@ tests = [
         , testCase "hour ago" testHourAgo
         , testCase "hours ago" testHoursAgo
         , testCase "day of week" testDow
+        , testCase "day of week in EDT" testDowEDT
+        , testCase "day of week in India" testDowIndia
         , testCase "days ago" testDaysAgo
         , testCase "week ago" testWeekAgo
         , testCase "weeks ago" testWeeksAgo
