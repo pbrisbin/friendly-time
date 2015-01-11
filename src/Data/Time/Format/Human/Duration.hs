303291fd81fba2a3dd6a94d5717779da3da6460c
patrick brisbin <pbrisbin@gmail.com>
2015-01-11 17:26:19 -0500

    Seconds and minutes passing

--- src/Data/Time/Format/Human/Duration.hs
+++ src/Data/Time/Format/Human/Duration.hs
@@ -5,11 +5,11 @@ module Data.Time.Format.Human.Duration
     , toDuration
     ) where
 
-import Data.Time (UTCTime)
+import Data.Time (NominalDiffTime, UTCTime, diffUTCTime)
 
 data Tense = Past deriving (Eq, Show)
 
-data Unit = Seconds deriving (Eq, Show)
+data Unit = Seconds | Minutes deriving (Eq, Show)
 
 data Duration = Duration
     { durationTense :: !Tense
@@ -18,4 +18,17 @@ data Duration = Duration
     } deriving (Eq, Show)
 
 toDuration :: UTCTime -> UTCTime -> Duration
-toDuration _ _ = Duration Past Seconds 0
+toDuration now = helper . diffUTCTime now
+  where
+    helper :: NominalDiffTime -> Duration
+    helper d
+        | d > -1 && d < 1 = Duration Past Seconds 0
+        | d > -60 && d < 60 = Duration Past Seconds $ toSeconds d
+        | d > -(60*60) && d < (60*60) = Duration Past Minutes $ toMinutes d
+        | otherwise = undefined
+
+    toSeconds :: NominalDiffTime -> Int
+    toSeconds = truncate
+
+    toMinutes :: NominalDiffTime -> Int
+    toMinutes s = toSeconds $ s / 60
--- test/Data/Time/Format/Human/DurationSpec.hs
+++ test/Data/Time/Format/Human/DurationSpec.hs
@@ -6,7 +6,10 @@ module Data.Time.Format.Human.DurationSpec
 import Test.Hspec
 import Data.Time.Format.Human.Duration
 
+import Data.Maybe (fromJust)
 import Data.Time (UTCTime)
+import Data.Time.Format (parseTime)
+import System.Locale (defaultTimeLocale)
 
 main :: IO ()
 main = hspec spec
@@ -14,14 +17,22 @@ main = hspec spec
 spec :: Spec
 spec = describe "Duration" $ do
     it "produces zero duration for near values" $ do
-        let (from, to) = makeTimes "1:00:00.000" "1:00:00.100"
+        let n = parseTime' "2015-01-01 01:00:00"
+            t = parseTime' "2015-01-01 01:00:00.1"
 
-        durationValue (toDuration from to) `shouldBe` 0
+        durationValue (toDuration n t) `shouldBe` 0
 
     it "produces seconds in past for duration of less than a minute" $ do
-        let (from, to) = makeTimes "1:00:59.000" "1:00:00.000"
+        let n = parseTime' "2015-01-01 01:00:59"
+        let t = parseTime' "2015-01-01 01:00:00"
 
-        toDuration from to `shouldBe` Duration Past Seconds 59
+        toDuration n t `shouldBe` Duration Past Seconds 59
 
-makeTimes :: String -> String -> (UTCTime, UTCTime)
-makeTimes = undefined
+    it "produces minutes in past for duration of less than an hour" $ do
+        let n = parseTime' "2015-01-01 01:59:00"
+        let t = parseTime' "2015-01-01 01:00:00"
+
+        toDuration n t `shouldBe` Duration Past Minutes 59
+
+parseTime' :: String -> UTCTime
+parseTime' = fromJust . parseTime defaultTimeLocale "%F %T%Q"
