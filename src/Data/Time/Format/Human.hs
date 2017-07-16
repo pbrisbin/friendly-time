9c6d8ba2979907a4cd372a2343c57f69da023f1b
Daniel Campoverde <alx@sillybytes.net>
2017-07-15 22:28:24 -0500

    Remove redundant import

--- src/Data/Time/Format/Human.hs
+++ src/Data/Time/Format/Human.hs
@@ -21,8 +21,6 @@ import Data.Char (isSpace)
 
 #if !MIN_VERSION_time(1,5,0)
 import System.Locale (TimeLocale, defaultTimeLocale)
-#else
-import Data.Time (TimeLocale, defaultTimeLocale)
 #endif
 
 
